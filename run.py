dcd1854094859ac3c51e1638ae513cda13672ae6
ar2189 <atif2189.ar@gmail.com>
2022-12-13 22:11:59 +0000

    Create a function to choose the size of the grid

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+
+def make_sea():
+    sea = []
+    valid_size = False
+    while valid_size == False:
+        try:
+            sea_size = int(
+                input("\nHow large would you like the sea? (2 - 8):")
+            )
+            if (sea_size < 2):
+                print("Sea too small, please enter again.")
+            elif (sea_size > 10):
+                print("Sea too large, please enter again")
+            else:
+                for _ in range(sea_size):
+                    sea.append([0] *sea_size)
+                valid_size = True
+        except ValueError:
+            print("Not an integer, please try agian.")
+    return sea
